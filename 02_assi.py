0db54391987f619aca0986f75a08d5328fc8e286
AjKing230 <119474802+AjKing230@users.noreply.github.com>
2022-11-30 10:39:22 +0530

    Add files via upload

---
+++ 02_assi.py
@@ -0,0 +1,4 @@
+a=int(input('Enter your Gross Income:'))
+b=int(input('Enter the number of Dependants:'))
+c=(a-10000-(b*3000))
+print('Tax:',c*0.2)
---
+++ 03_assi.py
@@ -0,0 +1,4 @@
+a=int(input('Enter the number of seconds:'))
+b=a//60
+c=a-(b*60)
+print('Seconds converted to minutes is '+ str(b) +"minutes" +str(c) +"seconds")
